25e5403e439db95019d994e2055a22a0dbc1755c
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2018-11-08 15:17:32 +0300

    Animation

---
+++ AnimationObject/Assets/Animation1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation1 : MonoBehaviour {
+
+
+    Animator m_animator;
+
+	// Use this for initialization
+	void Start () {
+        m_animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            m_animator.SetBool("Go", true);
+            Debug.Log("Animation Start");
+        }
+	}
+}
